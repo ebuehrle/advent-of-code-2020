6a1d6c8f01cb762abde27b01edd2202d0db096a0
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2020-12-10 10:57:34 +0100

    15 days to go...

---
+++ 10/p1.py
@@ -0,0 +1,23 @@
+import sys
+
+adapters = list(sorted(map(int, sys.stdin)))
+
+joltages = [0] + adapters + [adapters[-1] + 3]
+dist = [0, 0, 0, 0]
+for prv, nxt in zip(joltages[:-1], joltages[1:]):
+    dist[nxt - prv] += 1
+
+print(joltages)
+print(dist)
+print('P1', dist[1] * dist[3])
+
+cnt_ways = [0 for j in joltages]
+cnt_ways[-1] = 1
+for i in reversed(range(len(joltages[:-1]))):
+    v1 = joltages[i]
+    for j, v2 in enumerate(joltages[i+1:], i+1):
+        if v2 - v1 > 3:
+            break
+        cnt_ways[i] += cnt_ways[j]
+
+print('P2', cnt_ways[0])
