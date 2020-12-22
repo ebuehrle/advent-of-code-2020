3d0a4109fa315bf8274c4cdee4e1a8c1c6ff8386
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2020-12-22 09:21:07 +0100

    3 days to go...

--- 22/p2.py
+++ 22/p2.py
@@ -17,10 +17,10 @@ def parse_cards(src):
     return tuple(p1cards), tuple(p2cards)
 
 def score(deck):
-    card_scores = [(i+1)*c for i, c in enumerate(reversed(deck))]
+    card_scores = [i*c for i, c in enumerate(reversed(deck), 1)]
     return sum(card_scores)
 
-def combat(p1cards, p2cards):
+def recursive_combat(p1cards, p2cards):
     previous_hands = set()
 
     while p1cards and p2cards:
@@ -33,7 +33,7 @@ def combat(p1cards, p2cards):
         p2 = p2cards[0]
 
         if (len(p1cards) - 1 >= p1) and (len(p2cards) - 1 >= p2):
-            round_score = combat(p1cards[1:1+p1], p2cards[1:1+p2])
+            round_score = recursive_combat(p1cards[1:1+p1], p2cards[1:1+p2])
         else:
             round_score = (p1, p2)
 
@@ -50,5 +50,5 @@ def combat(p1cards, p2cards):
 if __name__ == '__main__':
     import sys
     p1cards, p2cards = parse_cards(open(sys.argv[1]))
-    score = combat(p1cards, p2cards)
+    score = recursive_combat(p1cards, p2cards)
     print(score)
